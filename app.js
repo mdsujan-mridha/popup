239d6fbda348fccccc3f4b634cc5cf3436d020c9
mdsujon-mridha <mdsujan134523@gmail.com>
2022-03-12 13:29:16 +0600

    admin

---
+++ app.js
@@ -0,0 +1,13 @@
+document.getElementById('open-pupup').addEventListener('click',function(){
+    const popUp=document.getElementById('popup');
+    popUp.style.display = "block";
+});
+
+document.getElementById('close').addEventListener('click',function(){
+    
+    const popUp=document.getElementById('popup');
+    popUp.style.display = "none";
+
+});
+
+
